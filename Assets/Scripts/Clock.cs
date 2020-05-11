368b08cd9179aee8752936658d0714726683f0c9
Oscar Freij <otg020313@gmail.com>
2020-05-11 10:36:05 +0200

    Added Basic Timer Logic + UI

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -13,9 +13,14 @@ public class Clock : MonoBehaviour
 
     public double TotalTimeLeft = 0;
     public double TotalTimeFinal = 0;
+    public double OldTotalTimeLeft = 0;
 
     public Image ClockRingFill;
     public Text CountDownText;
+
+    public GameObject ControlPanel;
+
+    public bool ClockIsRunning = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,12 +32,18 @@ public class Clock : MonoBehaviour
         TotalTimeFinal = TotalTimeLeft;
         ClockRingFill = transform.Find("Ring").transform.Find("Fill").GetComponent<Image>();
         CountDownText = transform.Find("CountDownText").GetComponent<Text>();
+        ControlPanel = transform.Find("ControlPanel").gameObject;
+
+        ControlPanel.transform.Find("Start_Pause").transform.Find("Text").GetComponent<Text>().text = "Start";
+        ControlPanel.transform.Find("Stop_Reset").transform.Find("Text").GetComponent<Text>().text = "Reset";
+
+        CountDownText.text = ($"00:00:00:00");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((float)TotalTimeLeft / (float)TotalTimeFinal > 0)
+        if ((float)TotalTimeLeft / (float)TotalTimeFinal > 0 && ClockIsRunning)
         {
             TotalTimeLeft -= Time.deltaTime / 60 / 60;
 
@@ -50,6 +61,89 @@ public class Clock : MonoBehaviour
 
             ClockRingFill.fillAmount = 1 - ((float)TotalTimeLeft / (float)TotalTimeFinal);
 
+            if (OldTotalTimeLeft < 0)
+            {
+                OldTotalTimeLeft = 0;
+            }
+            
+            if (TotalTimeLeft < 0)
+            {
+                TotalTimeLeft = 0;
+                CountDownText.text = ($"00:00:00:00");
+                ClockIsRunning = false;
+                ControlPanel.transform.Find("Start_Pause").transform.Find("Text").GetComponent<Text>().text = "Start";
+                ControlPanel.transform.Find("Stop_Reset").transform.Find("Text").GetComponent<Text>().text = "Reset";
+            }
+        }
+    }
+
+    public void StartPause()
+    {
+        if (!ClockIsRunning)
+        {
+
+            if (OldTotalTimeLeft == 0)
+            {
+                ControlPanel.transform.Find("Start_Pause").transform.Find("Text").GetComponent<Text>().text = "Pause";
+                ControlPanel.transform.Find("Stop_Reset").transform.Find("Text").GetComponent<Text>().text = "Stop";
+
+                try
+                {
+                    Hours = Int32.Parse(ControlPanel.transform.Find("TimePanel").transform.Find("InputFieldHH").transform.Find("Text").GetComponent<Text>().text);
+                }
+                catch
+                {
+                    Hours = 0;
+                }
+            
+                try
+                {
+                    Minutes = Int32.Parse(ControlPanel.transform.Find("TimePanel").transform.Find("InputFieldMM").transform.Find("Text").GetComponent<Text>().text);
+                }
+                catch
+                {
+                    Minutes = 0;
+                }
+
+                try
+                {
+                    Seconds = Int32.Parse(ControlPanel.transform.Find("TimePanel").transform.Find("InputFieldSS").transform.Find("Text").GetComponent<Text>().text);
+                }
+                catch
+                {
+                    Seconds = 0;
+                }
+
+                try
+                {
+                    Milliseconds = Int32.Parse(ControlPanel.transform.Find("TimePanel").transform.Find("InputFieldMS").transform.Find("Text").GetComponent<Text>().text);
+                }
+                catch
+                {
+                    Milliseconds = 0;
+                }
+
+                TotalTimeFinal = 0;
+                TotalTimeLeft += Hours;
+                TotalTimeLeft += Minutes / 60;
+                TotalTimeLeft += Seconds / (60 * 60);
+                TotalTimeLeft += Milliseconds / 3600000;
+                TotalTimeFinal = TotalTimeLeft;
+            }
+            else
+            {
+                TotalTimeLeft = OldTotalTimeLeft;
+            }
+
+
+            ClockIsRunning = true;
+        }
+        else
+        {
+            OldTotalTimeLeft = TotalTimeLeft;
+            ControlPanel.transform.Find("Start_Pause").transform.Find("Text").GetComponent<Text>().text = "Start";
+            ControlPanel.transform.Find("Stop_Reset").transform.Find("Text").GetComponent<Text>().text = "Reset";
+            ClockIsRunning = false;
         }
     }
 }
